867a9c55883b582310d44bd1323bbbc65515ff7d
Joshua <olajidejoshua4real@gmail.com>
2022-05-06 13:33:46 +0100

    Setup basic layout for checkout

--- src/pages/Checkout/Checkout.tsx
+++ src/pages/Checkout/Checkout.tsx
@@ -4,7 +4,15 @@ function Checkout() {
   return (
     <div className={Styles["Checkout"]}>
       <Header title="Checkout." />
-      <div className={Styles["Checkout__container"]}></div>
+      <div className={Styles["Checkout__container"]}>
+        <section className={Styles["Checkout__container--payment"]}>
+          <div className={Styles["delivery"]}></div>
+          <div className={Styles["payment"]}></div>
+        </section>
+        <section className={Styles["Checkout__container--total"]}>
+          <h1>Total Card</h1>
+        </section>
+      </div>
     </div>
   );
 }
